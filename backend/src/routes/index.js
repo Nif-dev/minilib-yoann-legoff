8c1a3aff4d6a7968af0cca5d87ba201c22e47173
Nif-dev <131044796+Nif-dev@users.noreply.github.com>
2026-03-20 15:34:31 +0100

    feat(adherent) : ajout des routes adherent a point entree api

--- backend/src/routes/index.js
+++ backend/src/routes/index.js
@@ -6,6 +6,7 @@ import express from 'express';
 // Import des routes spécifiques de l'api
 import healthRouter from './healthRouter.js';
 import livresRouter from './livresRoutes.js';
+import adherentsRouter from './adherentsRoutes.js';
 
 /**
  * Router - pour définir et organiser les routes de l'application.
@@ -19,5 +20,8 @@ const router = express.Router();
 
     // dirige les appels vers la route associé livresRouter
     router.use('/livres',livresRouter);
+
+    // dirige les appels vers la route associé adherentsRouter
+    router.use('/adherents',adherentsRouter);
 
 export default router;
