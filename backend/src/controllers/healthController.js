5f2c4712cb09822a02e14ef7a038dd29d531d012
Nif-dev <131044796+Nif-dev@users.noreply.github.com>
2026-03-25 12:15:00 +0100

    refactor(health) : route statut du serveur Typé

--- backend/src/controllers/healthController.js
+++ backend/src/controllers/healthController.js
@@ -1,11 +1,23 @@
 //% backend/src/routes/healthRouter.js
 //? Route de health check du serveur
 
+
+/**
+ * Route de health check du serveur
+ * 
+ * @import { Request, Response } from 'express';
+ * @import { ApiResponse } from '../types/index.js';
+ * @param {Request} req 
+ * @param {Response<ApiResponse<any>>} res 
+ */
 const healthController = (req, res) => {
     res.status(200).json({ 
-        status: 'OK',
-        timestamp: new Date().toISOString(),
-        message: 'Le serveur, il est vivant !' 
+        data: {
+            status: 'OK',
+            timestamp: new Date().toISOString(),
+        },
+        success: true,
+        message: 'Le serveur, il est vivant !'
     });
 }
 
