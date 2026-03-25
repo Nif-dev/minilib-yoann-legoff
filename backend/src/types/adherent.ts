3bef926ea8645a7fbd5f2b4935cd19ebc04dd012
Nif-dev <131044796+Nif-dev@users.noreply.github.com>
2026-03-25 10:22:03 +0100

    feat(types) : ajout typescript livre, adherent, emprunt

---
+++ backend/src/types/adherent.ts
@@ -0,0 +1,30 @@
+//% backend/src/types/adherents.ts
+//? Typage du Model Adherent
+
+/**
+ * Représente un adherent dans le catalogue de MiniLib
+ * Correspond aux colonnes de la table adherents dans PostgreSQL
+ *
+ * @interface Adherent
+ */
+export interface Adherent {
+    id:                 number;
+    numero_adherent:    string;
+    nom:                string;
+    prenom:             string;
+    email:              string;
+    actif:              boolean;
+    created_at:         Date;
+}
+
+/** 
+ * Données de création d'un nouvel adherent
+ * Pas d'id (serial) ni actif (par défaut = true)
+ *
+ * @interface CreateAdherentDTO
+*/
+export interface CreateAdherentDTO {
+    nom :           string;
+    prenom :        string;
+    email :         string;
+}
---
+++ backend/src/types/index.ts
@@ -0,0 +1,7 @@
+//% backend/src/types/index.ts
+//? Centralisation des types de l'application
+// Usage : `import { Livre, Adherent, CreateEmpruntDto } from '../types/index.ts';`
+
+export * from './livre.js';
+export * from './adherent.js';
+export * from './emprunt.js';
