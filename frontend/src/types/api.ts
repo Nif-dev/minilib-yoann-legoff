605653a06f47738e07b39e672cd6c1ec6a3f6610
Nif-dev <131044796+Nif-dev@users.noreply.github.com>
2026-04-02 14:50:35 +0200

    fix(types) : remis dans dossier mieux adapté

---
+++ frontend/src/types/api.ts
@@ -0,0 +1,18 @@
+//% frontend/src/types/api.ts
+//? Typage des réponses de l'API
+
+/**
+ * Wrapper / Interface pour toutes les réponses en sorties de l'API
+ * (success, data, message, error)
+*
+* @interface ApiResponse
+* @template T - Type de donnée attendu
+ */
+export interface ApiResponse<T> {
+    data:       T;
+    success?:   boolean;
+    message?:   string;
+    total?:     number; // pour les listes
+    error?:      string; // pour les erreurs 
+    champs?:    string[]; // champs manquants pour les 400
+}
--- frontend/src/types/index.ts
+++ frontend/src/types/index.ts
@@ -2,7 +2,7 @@
 //? Centralisation des types de l'API
 // Usage : `import { Livre, Adherent, CreateEmpruntDto } from '../types/index.ts';`
 
-
+export * from './api.ts';
 // types de données internes de l'application
 export * from './livre.ts';
 export * from './adherent.ts';
