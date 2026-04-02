905e4aa30ef177d9570bbecc7f05b0fc6c517d36
Nif-dev <131044796+Nif-dev@users.noreply.github.com>
2026-04-02 19:07:04 +0200

    fix(config) : ordre d'import de l'App, light mode par défaut

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -5,6 +5,7 @@ import { Outlet } from 'react-router-dom';
 
 import NavBar from './components/NavBar';
 
+import './App.css'
 function App() {
 
   return (
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -6,8 +6,6 @@ import ReactDOM from 'react-dom/client'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './Router.tsx'
 
-import './App.css'
-
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <RouterProvider router={router} />
