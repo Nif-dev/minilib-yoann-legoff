473d0d797e42de0ba3256b702e7f19ff2efaa7f8
Nif-dev <131044796+Nif-dev@users.noreply.github.com>
2026-04-06 14:02:10 +0200

    fix(livres) : retrait des filtres sur la requete principale, les filtres sont sur la requete recherche spécifique

--- frontend/src/services/api/livreService.ts
+++ frontend/src/services/api/livreService.ts
@@ -16,16 +16,19 @@ import { apiRequest } from './api';
  * Récupère tous les livres avec filtres optionnels.
  * @param { FiltresRechercheLivres } filtres - genre, disponible, recherche
  */
-export async function getLivres(filtres: FiltresRechercheLivres = {}): Promise<ApiResponse<Livre[]>> {
+export async function getLivres(
+    // filtres: FiltresRechercheLivres = {}
+): Promise<ApiResponse<Livre[]>> {
     // Construire les query params depuis les filtres 'non-undefined'
-    const params = new URLSearchParams();
-    if (filtres.genre)      params.append("genre", filtres.genre);
-    if (filtres.recherche)  params.append("recherche", filtres.recherche);
-    if (filtres.disponible  !== undefined)
-    params.append("disponible", String(filtres.disponible));
+    // const params = new URLSearchParams();
+    // if (filtres.genre)      params.append("genre", filtres.genre);
+    // if (filtres.recherche)  params.append("recherche", filtres.recherche);
+    // if (filtres.disponible  !== undefined)
+    // params.append("disponible", String(filtres.disponible));
 
-    const query = params.toString() ? `?${params.toString()}` : "";
-    return apiRequest<Livre[]>(`/livres${query}`);
+    // const query = params.toString() ? `?${params.toString()}` : "";
+    // return apiRequest<Livre[]>(`/livres${query}`);
+    return apiRequest<Livre[]>(`/livres`);
 }
 
 //TODO identifier selon cas d'usage la diff FRONT et BACK entre ces 2 approches
@@ -48,6 +51,8 @@ export async function queryLivres(filtres: FiltresRechercheLivres = {}): Promise
 
 /**
  * Récupère un livre par son id.
+ *
+ * @param { number } id - id du livre rechercher
  */
 export async function getLivreById(id: number): Promise<ApiResponse<Livre>> {
     return apiRequest<Livre>(`/livres/${id}`);
@@ -64,6 +69,8 @@ export async function creerLivre(data: CreateLivreDTO): Promise<ApiResponse<Livr
 }
 /**
  * Supprime un livre.
+ *
+ * @param { number } id - id du livre à supprimer
  */
 export async function supprimerLivre(id: number): Promise<ApiResponse<void>> {
     return apiRequest<void>(`/livres/${id}`, { method: "DELETE" });
