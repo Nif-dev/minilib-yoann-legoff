b3fef9905a2a4ff6fb18bc553a74fd2ea056990f
Nif-dev <131044796+Nif-dev@users.noreply.github.com>
2026-04-10 15:36:49 +0200

    feature(livres) : ajout de modals pour nouveau livre, modification d'un existant, affichage retour API pour UX

--- frontend/src/components/ModalLivreModifier.tsx
+++ frontend/src/components/ModalLivreModifier.tsx
@@ -1,18 +1,23 @@
-//% frontend/src/components/ModalLivreAjout.tsx
-//? Formulaire de création d'un nouveau livre
+//% frontend/src/components/ModalLivreModifier.tsx
+//? Formulaire de modification d'un livre existant
 
 import { useState } from "react";
 
-import type { CreateLivreDTO } from "../types/index";
-import { creerLivre } from "../services/api/livreService";
+import type { Livre, UpdateLivreDTO } from "../types/index";
+import { modifierLivre } from "../services/api/livreService";
 
-interface ModalLivreAjoutProps {
+interface ModalLivreModifierProps {
     
     readonly isOpen: boolean;
     readonly onClose: () => void;
+    readonly oldLivre: Livre; // id du livre à modifier
 }
 
-export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProps) {
+export default function ModalLivreModifier({ 
+    isOpen, 
+    onClose, 
+    oldLivre 
+}: ModalLivreModifierProps) {
 
     const [chargement, setChargement] = useState<boolean>(false);
     const [message, setMessage] = useState<string | null>(null);
@@ -31,41 +36,52 @@ export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProp
         setChamps([]);
     }
 
+    const resetLivre = () => {
+        setISBN(oldLivre.isbn);
+        setTitre(oldLivre.titre);
+        setAuteur(oldLivre.auteur);
+        setGenre(oldLivre.genre);
+        setAnnee(oldLivre.annee);
+    }
+
+    const idLivre = oldLivre.id;
+
     // Données du formulaire
-    const [isbn, setISBN] = useState('');
-    const [titre, setTitre] = useState('');
-    const [auteur, setAuteur] = useState('');
-    const [genre, setGenre] = useState('');
-    const [annee, setAnnee] = useState(0);
+    const [isbn, setISBN] = useState(oldLivre.isbn);
+    const [titre, setTitre] = useState(oldLivre.titre);
+    const [auteur, setAuteur] = useState(oldLivre.auteur);
+    const [genre, setGenre] = useState(oldLivre.genre);
+    const [annee, setAnnee] = useState(oldLivre.annee);
     
-    
-    const newLivre: CreateLivreDTO = {
+    // Données de modification du livre
+    const newLivre: UpdateLivreDTO = {
         isbn: isbn,
         titre: titre,
         auteur: auteur,
         genre: genre,
-        annee: annee,
+        annee: annee.toString(),
     }
+
 
     const ajouterLivre = async () => {
         clearInfos();
         setChargement(true);
         await new Promise(resolve => setTimeout(resolve, 1000));
         try {
-            const response = await creerLivre(newLivre);
+            const response = await modifierLivre(idLivre, newLivre);
 
             if (!response.success) {
                 setChargement(false);
                 setErreur(response.error ?? "Erreur inconnue"); 
                 // message renvoyé par l'API - typiquement un message d'erreur 
-                setErrorMessage((response.message ?? response.error ?? '') + " Livre non ajouté"); 
+                setErrorMessage(response.message ?? response.error ?? "Livre non modifié"); 
                 // récupération des champs de formulaires fautifs
                 if (response.champs) {
                     setChamps(response.champs);
                 }
                 return;
             }
-            setMessage(response.message ?? 'Livre ajouté');
+            setMessage(response.message ?? 'Livre modifié');
             // autres traitements sur la réponse api si besoin
 
         } catch (err) {
@@ -81,7 +97,10 @@ export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProp
             <div className="modal-card">
 
                 <header className="modal-card-head">
-                    <p className="modal-card-title">Ajout d'un nouveau livre</p>
+                    <p className="modal-card-title">
+                        Modification du livre {oldLivre.titre},
+                        <span className="has-text-grey is-small">id : {oldLivre.id}</span>
+                    </p>
                     <button type="button" className="delete" aria-label="close" onClick={onClose}>
                     </button>
                 </header>
@@ -92,8 +111,9 @@ export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProp
                     <div className="field">
                         <label htmlFor="titre_field" className="label">Titre</label>
                         <div id="titre_field" className="control">
-                            <input className="input" type="text" required placeholder="Titre du livre..."
-                                onChange={(e) => setTitre(e.target.value)} />
+                            <input className="input" type="text" placeholder="Titre du livre..."
+                                onChange={(e) => setTitre(e.target.value)} 
+                                value={titre}/>
                         </div>
                         <p className="help is-danger ml-3"></p>
                     </div>
@@ -101,8 +121,9 @@ export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProp
                     <div className="field">
                         <label htmlFor="auteur_field" className="label">Auteur</label>
                         <div id="auteur_field" className="control">
-                            <input className="input" type="text" required placeholder="Auteur du livre..."
-                                onChange={(e) => setAuteur(e.target.value)} />
+                            <input className="input" type="text" placeholder="Auteur du livre..."
+                                onChange={(e) => setAuteur(e.target.value)}
+                                value={auteur} />
                         </div>
                         <p className="help is-danger ml-3"></p>
                     </div>
@@ -110,8 +131,9 @@ export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProp
                     <div className="field">
                         <label htmlFor="isbn_field" className="label">ISBN</label>
                         <div id="isbn_field" className="control">
-                            <input className="input" type="text" required placeholder="ISBN du livre..." 
-                                onChange={(e) => setISBN(e.target.value)} />
+                            <input className="input" type="text" placeholder="ISBN du livre..." 
+                                onChange={(e) => setISBN(e.target.value)} 
+                                value={isbn}/>
                         </div>
                         <p className="help is-danger ml-3"></p>
                     </div>
@@ -120,7 +142,8 @@ export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProp
                         <label htmlFor="genre_field" className="label">Genre</label>
                         <div id="genre_field" className="control">
                             <input className="input" type="text" placeholder="Genre du livre..." 
-                                onChange={(e) => setGenre(e.target.value)} />
+                                onChange={(e) => setGenre(e.target.value)} 
+                                value={genre}/>
                         </div>
                         <p className="help is-danger ml-3"></p>
                     </div>
@@ -128,9 +151,9 @@ export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProp
                     <div className="field">
                         <label htmlFor="annee_field" className="label">Année</label>
                         <div id="annee_field" className="control">
-                            <input className="input" type="number" min="-1000"
-                            max={new Date().getFullYear()} placeholder="Année de publication..." 
-                                onChange={(e) => setAnnee(Number(e.target.value))} />
+                            <input className="input" type="number" placeholder="Année de publication..." 
+                                onChange={(e) => setAnnee(Number.parseInt(e.target.value))} 
+                                value={annee}/>
                         </div>
                         <p className="help is-danger ml-3"></p>
                     </div>
@@ -142,8 +165,6 @@ export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProp
                         {message && <div className="has-text-success has-text-centered my-4">{message}</div>}
                         
                         {errorMessage && <div className="has-text-danger has-text-centered my-4">{errorMessage}</div>}
-                        
-                        
                         
                         {champs.length > 0 && <div className="has-text-danger has-text-left my-4">
                             <ul>
@@ -161,27 +182,33 @@ export default function ModalLivreAjout({ isOpen, onClose }: ModalLivreAjoutProp
                     <div className="field is-grouped">
                         <div className="control buttons">
                             
+                            {/* si erreur, bouton pour inciter à corriger */}
                             {erreur
                             ? <button type="button"
                                 disabled={chargement} 
                                 className="button is-warning " 
                                 onClick={() => clearInfos()}
-                                >
-                                Correction OK ?
+                                >Correction OK ?
                             </button>
                             : <button type='button' 
-                                disabled={erreur !== null || chargement || titre === '' || auteur === '' || isbn === ''} 
+                                disabled={erreur !== null || chargement } 
                                 className={erreur ? "button is-danger is-inactive" : "button is-success"} 
                                 onClick={() => ajouterLivre()}
-                                >
-                                Ajouter le livre !
+                                >Modifier le livre
                             </button>
                             }
+                            {/* bouton de reset du formulaire */}
+                            <button type="button" 
+                                disabled={chargement} 
+                                className="button is-warning" 
+                                onClick={resetLivre}
+                                >Reset
+                            </button>
+                            {/* bouton de fermeture du formulaire */}
                             <button type="button" 
                                 onClick={onClose} 
                                 className="button is-link is-light"
-                                >
-                                Fermer
+                                >Fermer
                                 </button>
                         
                         </div>
